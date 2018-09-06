5e5dc2e47b5703fa7baab36599a3287835ceca94
Abhilash <abhilash@heidelsoft-technologies.de>
2018-09-06 17:48:51 +0530

    Simple calculator

--- app.py
+++ app.py
@@ -1,7 +1,5 @@
-print("/___|")
-print("   /|")
-print("  / |")
-print(" /  |")
+from math import *
 
-
+user_name = input("Enter your Name: ")
+print(user_name + "'s Dashboard!")
 
---
+++ calculator.py
@@ -0,0 +1,5 @@
+# num1 = input("Enter the number: ")
+# num2 = input("Enter the number: ")
+# result = round(float(num1) + float(num2))
+# print("Result is: " + str(result))
+
